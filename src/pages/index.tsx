3a8f90f9234366545e96e052a5aa71a54aec7ce1
David <daavid.as11@gmail.com>
2023-11-23 18:57:45 -0300

    finish

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,147 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className="mt-2 mb-6">
+        <h1 className="text-2xl font-semibold text-gray-900 dark:text-white">
+          FUNDAMENTOS HISTÓRICOS
+        </h1>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <ol className="relative border-s border-gray-200 dark:border-gray-700">
+        <li className="mb-20 ms-16">
+          <span className="absolute flex items-center justify-center w-20 h-20 bg-blue-100 rounded-full -start-10 -y-3 ring-8 ring-white overflow-hidden">
+            <Image src="/imgs/piaget.jpg" alt="Jean Piaget" fill />
+          </span>
+          <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            Jean Piaget
+          </h3>
+          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            1896
+          </time>
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            Psicólogo suíço; teoria do desenvolvimento cognitivo; estágios de
+            desenvolvimento infantil; influência marcante na psicologia e
+            educação.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+        </li>
+        <li className="mb-20 ms-16">
+          <span className="absolute flex items-center justify-center w-20 h-20 bg-blue-100 rounded-full -start-10 -y-3 ring-8 ring-white overflow-hidden">
+            <Image src="/imgs/maria.jpg" alt="Maria Montessori" fill />
+          </span>
+          <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            Maria Montessori
+          </h3>
+          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            1907
+          </time>
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            Médica italiana pioneira em educação; desenvolveu método centrado na
+            autonomia e aprendizado prático, influenciando educação global.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
+        </li>
+        <li className="mb-20 ms-16">
+          <span className="absolute flex items-center justify-center w-20 h-20 bg-blue-100 rounded-full -start-10 -y-3 ring-8 ring-white overflow-hidden">
+            <Image src="/imgs/john.jpg" alt="John Dewey" fill />
+          </span>
+          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            John Dewey
+          </h3>
+          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            1916
+          </time>
+          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+            Filósofo, psicólogo e educador dos EUA; defensor da educação
+            progressiva e experiencial; influente na teoria educacional e
+            reforma escolar mundial.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        </li>
+        <li className="mb-20 ms-16">
+          <span className="absolute flex items-center justify-center w-20 h-20 bg-blue-100 rounded-full -start-10 -y-3 ring-8 ring-white overflow-hidden">
+            <Image src="/imgs/paulo.png" alt="Paulo Freire" fill />
+          </span>
+          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            Paulo Freire
+          </h3>
+          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            1980
+          </time>
+          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+            Educador brasileiro; pedagogia crítica; alfabetização como
+            libertação; engajamento social e educação como instrumentos de
+            transformação e conscientização.
           </p>
-        </a>
-      </div>
+        </li>
+        <li className="mb-20 ms-16">
+          <span className="absolute flex items-center justify-center w-20 h-20 bg-blue-100 rounded-full -start-10 -y-3 ring-8 ring-white overflow-hidden">
+            <Image src="/imgs/darcy.jpg" alt="Darcy Ribeiro" fill />
+          </span>
+          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            Darcy Ribeiro
+          </h3>
+          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            1922
+          </time>
+          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+            Antropólogo e político brasileiro; defensor da educação; idealizou
+            projetos educacionais, destacou-se na valorização da cultura e
+            identidade latino-americanas.
+          </p>
+        </li>
+        <li className="mb-20 ms-16">
+          <span className="absolute flex items-center justify-center w-20 h-20 bg-blue-100 rounded-full -start-10 -y-3 ring-8 ring-white overflow-hidden">
+            <Image src="/imgs/lev.jpg" alt="Lev Vygotsky" fill />
+          </span>
+          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            Lev Vygotsky
+          </h3>
+          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            1924
+          </time>
+          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+            Psicólogo russo; teoria sociocultural enfatiza influência cultural
+            no desenvolvimento; zona proximal desenvolvimento destaca interação
+            social e aprendizado.
+          </p>
+        </li>
+        <li className="mb-20 ms-16">
+          <span className="absolute flex items-center justify-center w-20 h-20 bg-blue-100 rounded-full -start-10 -y-3 ring-8 ring-white overflow-hidden">
+            <Image src="/imgs/anisio.jpg" alt="Anísio Teixeira" fill />
+          </span>
+          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            Anísio Teixeira
+          </h3>
+          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            1931
+          </time>
+          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+            Educador brasileiro; pioneiro na educação pública; idealizou escolas
+            modernas, defendeu acesso amplo à educação e reformas educacionais
+            significativas no Brasil.
+          </p>
+        </li>
+        <li className="mb-20 ms-16">
+          <span className="absolute flex items-center justify-center w-20 h-20 bg-blue-100 rounded-full -start-10 -y-3 ring-8 ring-white overflow-hidden">
+            <Image src="/imgs/pierre.jpg" alt="Pierre Bourdieu" fill />
+          </span>
+          <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+            Pierre Bourdieu
+          </h3>
+          <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            1970
+          </time>
+          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+            Sociólogo francês; teoria da reprodução social destaca influência
+            cultural e capital simbólico; investigou desigualdades sociais e
+            cultura na sociedade contemporânea.
+          </p>
+        </li>
+      </ol>
     </main>
-  )
+  );
 }
